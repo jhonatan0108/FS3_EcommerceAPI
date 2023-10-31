4d9de8b5691adaa567de70f6d41a3f15fefaa7e2
DTProdesign <dtgraphicdesigner@gmail.com>
2023-10-31 15:50:17 -0500

    Entidades Cliente, Producto y controller

--- EcommerceAPI.Infraestructura/Database/Entidades/ClienteEntity.cs
+++ EcommerceAPI.Infraestructura/Database/Entidades/ClienteEntity.cs
@@ -9,9 +9,9 @@ namespace EcommerceAPI.Infraestructura.Database.Entidades
         [Key]
         public int id_cliente { get; set; }
         public string nombre { get; set; }
-        public string contrasena { get; set; }
+        public string password { get; set; }
         public string correo { get; set; }
-        public string direccioncliente { get; set; }
-        public int telefono { get; set; }
+        public string direccionfacturacion { get; set; }
+        public decimal telefono { get; set; }
     }
 }
