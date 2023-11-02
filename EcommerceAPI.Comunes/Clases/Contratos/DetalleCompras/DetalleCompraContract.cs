d4ffe085e1bb182a60c72fffe6c57517a4c7e821
DTProdesign <dtgraphicdesigner@gmail.com>
2023-11-01 20:12:59 -0500

    Arreglo en base de datos clienteentity

--- EcommerceAPI.Comunes/Clases/Contratos/DetalleCompras/DetalleCompraContract.cs
+++ EcommerceAPI.Comunes/Clases/Contratos/DetalleCompras/DetalleCompraContract.cs
@@ -4,7 +4,7 @@
     {
         public int id_detallecompra { get; set; }
         public int cantidad { get; set; }
-        public string valorunitario { get; set; }
+        public double valorunitario { get; set; }
         public int? id_compra { get; set; }
         public int id_producto { get; set; }
 
--- EcommerceAPI.Infraestructura/Database/Entidades/CompraEntity.cs
+++ EcommerceAPI.Infraestructura/Database/Entidades/CompraEntity.cs
@@ -8,20 +8,15 @@ namespace EcommerceAPI.Infraestructura.Database.Entidades
     public class CompraEntity
     {
         [Key]
-        [ForeignKey("id_client")]
-        public int id_compras { get; set; }
+        
+        public int id_compra { get; set; }
         public DateTime fecha { get; set; }
         public double valortotal { get; set; }
         public string? direccionentrega { get; set; }
-        //public int id_cliente { get; set; }//
+        public int id_cliente { get; set; }
         public int id_estado { get; set; }
 
-        public int Id { get; set; }
-        // otras propiedades...
-
-        public int id_client { get; set; } // Esta es la clave externa
-
-        
+                
         
     }
 
