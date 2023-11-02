b85a63482ff513d1aaaad3aebd2c9dcdce2fea07
DTProdesign <dtgraphicdesigner@gmail.com>
2023-11-02 13:54:48 -0500

    Cifrado Helper

---
+++ EcommerceAPI.Comunes/Clases/Helpers/Cifrado/CifradoHelper.cs
@@ -0,0 +1,27 @@
+﻿namespace EcommerceAPI.Comunes.Clases.Helpers.Cifrado
+{
+    public static class CifradoHelper
+    {
+        ///Escriba una cadena
+
+        public static string Encriptar(this string _cadenaAencriptar)
+        {
+            string result = string.Empty;
+            byte[] encrytde = System.Text.Encoding.Unicode.GetBytes(_cadenaAencriptar);
+            result = Convert.ToBase64String(encrytde);
+            return result;
+        }
+
+        ///Esta función desencripta la cadena que le enviamos en el parámetro de entrada
+
+        public static string Desencriptar(this string _cadenaAdesencriptar)
+        {
+            string result = string.Empty;
+            byte[] decrytde = System.Text.Encoding.Unicode.GetBytes(_cadenaAdesencriptar);
+            //result = System.Text.Encoding.Unicode.GetString(decryted, 0, decryted.ToArray().Length);
+            result = System.Text.Encoding.Unicode.GetString(decrytde);
+            return result;
+        }
+
+    }
+}
